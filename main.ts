766ec59acaffe1a05044fa3a80f9e82b3130d0a2
Zane Shaw <zane@squidee.dev>
2026-01-30 01:36:20 +1100

    improve directive parsing

--- main.ts
+++ main.ts
@@ -1,15 +1,24 @@
 import config from "./config.json";
 import { join } from "path";
 
+type Directive = {
+	type: "include" | "layout" | "block" | "endblock";
+	args: { [key: string]: string };
+	startIndex: number;
+	endIndex: number;
+};
+
+let layout: string | undefined;
+
 // https://stackoverflow.com/a/40782646
-function matchBetweenAll(str: string, from: string, to: string) {
-	const regex = new RegExp(`(?<=${from}\s*).*?(?=\s*${to})`, "gs");
+function matchBetweenAll(str: string, from: string, to: string, inner: boolean = true) {
+	const regex = new RegExp(`(${inner ? "?<=" : ""}${from}\s*).*?(${inner ? "?=" : ""}\s*${to})`, "gs");
 
 	return str.match(regex)?.map((x) => x.trim()) || [];
 }
 
-function matchBetween(str: string, from: string, to: string) {
-	const regex = new RegExp(`(?<=${from}\s*).*?(?=\s*${to})`, "s");
+function matchBetween(str: string, from: string, to: string, inner: boolean = true) {
+	const regex = new RegExp(`(${inner ? "?<=" : ""}${from}\s*).*?(${inner ? "?=" : ""}\s*${to})`, "s");
 
 	return str.match(regex)?.[0].trim();
 }
@@ -19,16 +28,38 @@ function replaceBetween(targetStr: string, srcStr: string, from: number, to: num
 	return targetStr.substring(0, from) + srcStr + targetStr.substring(to);
 }
 
-async function resolveIncludeDirective(html: string, directive: string) {
-	const path = matchBetween(directive, 'file="', '"');
+function parseDirectives(html: string) {
+	let cursor = 0;
+	const directives: Directive[] = matchBetweenAll(html, "<!--#", "-->", false).map((directive) => {
+		const startIndex = html.indexOf(directive, cursor);
+		const content = matchBetween(directive, "<!--#", "-->")!;
+		const args: { [key: string]: string } = {};
 
-	if (path) {
-		const file = Bun.file(join(config.public_path, path));
+		for (const arg of content.split(" ").slice(1)) {
+			const key = arg.split("=")[0]!;
+			const value = arg.split("=")[1]?.slice(1, -1)!;
+			args[key] = value;
+		}
+
+		cursor = startIndex + directive.length;
+
+		return {
+			type: content.split(" ")[0],
+			args: args,
+			startIndex: startIndex,
+			endIndex: startIndex + directive.length,
+		} as Directive;
+	});
+
+	return directives;
+}
+
+async function resolveIncludeDirective(html: string, directive: Directive) {
+	if (directive.args.file) {
+		const file = Bun.file(join(config.public_path, directive.args.file));
 		const includeHtml = await file.text();
 
-		const from = html.indexOf("<!--# include");
-		const to = html.indexOf("-->", from) + 3;
-		const patchedHtml = replaceBetween(html, includeHtml, from, to);
+		const patchedHtml = replaceBetween(html, includeHtml, directive.startIndex, directive.endIndex);
 
 		return patchedHtml;
 	}
@@ -36,15 +67,20 @@ async function resolveIncludeDirective(html: string, directive: string) {
 	return html;
 }
 
-async function resolveDirectives(html: string) {
-	const directives = new Set(matchBetweenAll(html, "<!--#", "-->"));
+async function resolveBlockDirective(html: string, directive: string, endDirective: string) {
+	console.log(html.indexOf(directive), html.indexOf(endDirective));
 
-	for (const directive of directives) {
-		const directiveType = directive.split(" ")[0];
+	return html;
+}
 
-		if (directiveType == "include") {
+async function resolveDirectives(html: string, directives: Directive[]) {
+	// loop just looks for the first handled directive
+	for (let i = 0; i < directives.length; i++) {
+		const directive = directives[i]!;
+
+		if (directive.type == "include") {
 			const resolved = await resolveIncludeDirective(html, directive);
-			return resolveDirectives(resolved);
+			return resolveDirectives(resolved, directives.toSpliced(i, 1));
 		}
 	}
 
@@ -65,7 +101,10 @@ Bun.serve({
 		if (dest == "document") {
 			const path = url.pathname == "/" ? "index.html" : url.pathname;
 			const file = Bun.file(join(config.public_path, path));
-			const html = await resolveDirectives(await file.text());
+			let html = await file.text();
+
+			const directives = parseDirectives(html);
+			html = await resolveDirectives(html, directives);
 
 			return new Response(html, { headers: { "Content-Type": "text/html" } });
 		} else if (dest == "image" || dest == "script" || dest == "font" || dest == "iframe") {
@@ -77,3 +116,5 @@ Bun.serve({
 		return new Response(null, { status: 404 });
 	},
 });
+
+console.log(`server started on port ${config.port}`);
